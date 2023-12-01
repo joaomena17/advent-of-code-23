1a710925a8f9af296b32876f6fa4d23b0a79213c
joaomena <joao.17.2001@gmail.com>
2023-12-01 13:43:13 +0000

    added file opening and line by line reading, missing line processing logic

--- day-01/trebuchet.go
+++ day-01/trebuchet.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func processLine(line string) int {
+	// return dummy data to test, will add logic later
+	return 77
+}
 
 func main() {
-  fmt.Println("Advent of Code - Day 1\n")
+	// path for file with input data
+	input_file := "input.txt"
+
+	// open file
+	fp, err := os.Open(input_file)
+
+	// handle error
+	if err != nil {
+		fmt.Println("Error oprning file: ", err)
+		return
+	}
+
+	defer fp.Close() // ensures file is closed when main function is done or if there is an error in processing
+
+	// create buffer to read lines in the file
+	lineReader := bufio.NewScanner(fp)
+
+	// create global sum variable
+	var totalSum int = 0
+
+	// loop through the lines using the buffer
+	for lineReader.Scan() {
+
+		line := lineReader.Text()
+
+		var calibrationValue int = processLine(line)
+
+		totalSum += calibrationValue
+	}
+
+	// handle errors
+	if err := lineReader.Err(); err != nil {
+		fmt.Println("Error while reading file: ", err)
+		return
+	}
+
+	fmt.Println("The sum of all calibration values is: ", totalSum)
 }
